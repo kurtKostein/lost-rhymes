b8dbb94f9811ca94d9c3ea4d6fa01ab650a98380
Kurt Kostein <kurtkostein@yandex.ru>
2021-06-05 23:07:08 +0300

    install django-environ add it in settings.py

--- lost_rhymes/settings.py
+++ lost_rhymes/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import environ
+
 from pathlib import Path
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^our32ui78v_-a0k$a(dto#j9)h_evq3#@w3z7683%w5wo0=wo'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
