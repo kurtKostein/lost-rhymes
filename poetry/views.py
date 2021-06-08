e782ead13939321a101d7ca40c175dcce0ed8df2
Kurt Kostein <kurtkostein@yandex.ru>
2021-06-08 08:43:13 +0300

    add IndexPage(TemplateView) in head views

--- poetry/views.py
+++ poetry/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+#  from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+class IndexPage(TemplateView):
+    template_name = 'index.html'
